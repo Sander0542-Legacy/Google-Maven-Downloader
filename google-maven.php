75ef1c1c1ad9b9a43824c7dea99da79d403ffdac
Sander Jochems <sanderjochems@hotmail.nl>
2018-02-01 13:23:28 +0100

    Update google-maven.php

--- google-maven.php
+++ google-maven.php
@@ -88,7 +88,7 @@ if (isset($_GET["group"]) && isset($_GET["lib"]) && isset($_GET["version"])) {
           $groupIndex = str_replace(".","/",$dependency->groupId);
           $onclickURL = '/google-maven?group='.$groupIndex.'&lib='.$dependency->artifactId.'&version='.$dependency->version;
           echo '
-                  <div'; if ($AARExists) { echo ' onclick="window.location.href=\''.$onclickURL.'\'"'; }  echo ' class=" col l3 clickable">
+                  <div onclick="window.location.href=\''.$onclickURL.'\'" class=" col l3 clickable">
                     <div class="card hoverable grey lighten-4">
                       <div class="card-content center">
                         <h5>'.$dependency->artifactId.'</h5>
